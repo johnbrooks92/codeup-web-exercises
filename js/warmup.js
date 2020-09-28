cf6733fbb2486a9da2967757386577fb701cb8e4
John <johntbrooks92@gmail.com>
2020-09-28 09:32:41 -0500

    WarmUp Pt 2 9.28.2020

--- js/warmup.js
+++ js/warmup.js
@@ -1,5 +1,15 @@
+"use strict";
+
 console.log("Hello Connor")
 console.log("Hello Amber")
 console.log("Hello Matt")
 console.log("Hello Evan")
 console.log("Hello Caleb")
+
+fucntion sayHello(name){
+    console.log("Hello " + name);
+    // we will talk about returning later!
+}
+
+sayHello(name: "Connor");
+sayHello(name: "Amber");
