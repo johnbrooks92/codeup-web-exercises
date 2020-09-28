00c2fe12b250631cb5f889b6726cee2a96c9a8ac
John <johntbrooks92@gmail.com>
2020-09-28 12:04:57 -0500

    FinalExerciseAttempted

--- js/external.js
+++ js/external.js
@@ -1,14 +1,20 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
+
+// Use the alert function to show a message that says 'Welcome to my Website!'.
 alert("Welcome to my Website!")
 
+// Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the color entered is your favorite color too.
 var colorPrompt = prompt("What is your favorite color?");
 console.log(colorPrompt);
 console.log(typeof colorPrompt);
 
 alert("Great, " + colorPrompt + " is my favorite color, too!");
 
+// Complete exercise 3 from the previous lesson, but write your code in the external.js file instead of in the console.
+// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+// Use an alert to show the results of each problem.
 
 var mermaidDays = prompt("How many days did you rent The Little Mermaid for?");
 console.log(mermaidDays);
@@ -59,6 +65,18 @@ if (parseInt(studentNumber) <= maxStudents && parseInt(studentAvailable) >= minH
 
 if (parseInt(studentNumber) >= maxStudents && parseInt(studentAvailable) <= minHours)
     alert("You have NOT been enrolled into the course!")
+
+var itemsBought = parseInt(prompt("How many items are you purchasing? "));
+var offer = confirm("If this offer is expired, press Cancel ; If not, press OK.");
+var premiumMember = confirm("If you ARE a member, select OK ; Otherwise, select Cancel.")
+var validOffer = (itemsBought > 2 && !premiumMember && offer)
+var premiumMemberOffer = (premiumMember && offer);
+if (validOffer || premiumMemberOffer) {
+    alert("You got 20% off!");
+}else {
+        alert("This offer is invalid.")
+    }
+
 
 
 
