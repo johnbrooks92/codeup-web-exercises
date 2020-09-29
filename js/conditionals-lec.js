e1764f74f32a3eabf2cf081638ba64ce54d6ffef
John <johntbrooks92@gmail.com>
2020-09-29 14:16:34 -0500

    conditionals push 3

--- js/conditionals-lec.js
+++ js/conditionals-lec.js
@@ -67,56 +67,156 @@ console.log("hello from conditionals lec.js!");
 // }
 //
 //
+// //
+// // //TODO: Write an if statement that alerts "It's snowing!" if weather is equal to "snowing"
+// //
+// // var weather = "snowing";
+// // if(weather=== "snowing"){
+// //     alert("It's Snowing!");
+// // }
+// //
+// //
+// //
+// // //TODO: Write an if statement that alerts true if numberInput is greater than but not equal to 10
+// // var numberInput = 5
+// // if(numberInput > 10);{
+// //     alert("True. Number is greater than 10.")
+// // }
+// //
+// //
+// // //TODO: EXTRA BONUS - Refactor each of the previous statements as functions.
+// //
+// //
+// //
+// //
+// // // =============== IF / ELSE SYNTAX ================
+// //
+// // // if(condition){
+// // //     // code here runs if condition evaluates to true
+// // // } else {
+// // //     // code here runs if condition evaluates to false
+// // // }
+// //
 //
-// //TODO: Write an if statement that alerts "It's snowing!" if weather is equal to "snowing"
+//
+// // =============== IF / ELSE STATEMENT EXAMPLES ================
+// //TODO Together: Show a specific navbar if user is admin, else show a different navbar
+//
+// var isAdmin = false;
+//
+//     if(isAdmin){
+//         //show the admin navbar
+//         console.log("is admin")
+//     } else {
+//         //show regular navbar
+//         console.log("regular user")
+//     }
+//
+// //TODO Together: Write an if statement that alerts "It's raining" if isRainy is true, else return "have a nice day!"
+//
+// var isRainy = true;
+//
+// if(isRainy){
+//     alert("It's raining!")
+// } else {
+//     alert("Have a nice day!")
+// }
+//
+//
+//
+// //TODO: Write an if statement that alerts "Sorry, game over" if numberOfLives is 0, else alert "Next Level!"
+//
+// var numberOfLives = 5;
+//
+// if(numberOfLives === 0){
+//     alert("Sorry, game over!")
+// } else {
+//     alert("Next Level!")
+// }
+//
+//
+// //TODO: Write an if statement that alerts "It's snowing!" if weather is equal to "snowing", else alert "Check back later for more details!"
 //
 // var weather = "snowing";
-// if(weather=== "snowing"){
-//     alert("It's Snowing!");
+//
+// if(weather === "snowing"){
+//     alert("It's snowing!")
+// } else {
+//     alert("Check back later for more details!")
 // }
 //
 //
 //
-// //TODO: Write an if statement that alerts true if numberInput is greater than but not equal to 10
-// var numberInput = 5
-// if(numberInput > 10);{
-//     alert("True. Number is greater than 10.")
+//
+// //TODO: Write an if statement that alerts true if numberInput is greater than 10, else alert "the number is less than 10"
+//
+// var numberInput = 75
+//
+// if (numberInput > 10) {
+//     alert("The number is greater than 10.")
+// } else {
+//     alert ("The number is less than 10.")
+// }
+//
+// //TODO: Refactor todo#1 as a function named checkIfGameIsOver, that accepts numberOfLives and returns "Sorry, game over" if numberOfLives is 0, else return "Next Level!.
+//
+// function checkIfGameIsOver(numberOfLives) {
+//     if(numberOfLives ===0) {
+//         return "Sorry game over!";
+//     } else {
+//         return "Next level!"
+//     }
 // }
 //
 //
-// //TODO: EXTRA BONUS - Refactor each of the previous statements as functions.
+//
+// //TODO: EXTRA BONUS - Refactor todo#2 from above as a function.
 //
 //
 //
+// // SHOULD WE DELETE STUFF EXAMPLE
+// var weShouldDeleteStuff = confirm("Are you sure you want to delete everything?")
 //
-// // =============== IF / ELSE SYNTAX ================
+// // ok selection ==> true, cancel selection ==> false
 //
-// // if(condition){
+// if(weShouldDeleteStuff){
+//     // delete everything
+//     // double check
+//     alert("We are deleting everything...");
+// } else {
+//     alert("Operation cancelled.")
+// }
+//
+//
+//
+// //TODO: Write a confirm asking the user if they are 13 years of age or older. If they are, alert "You may proceed", if they are not, alert "Sorry, you are not able to proceed"
+//
+// // ================ IF / ELSE IF / ELSE STATEMENT SYNTAX ===============
+// // if(condition1){
 // //     // code here runs if condition evaluates to true
-// // } else {
-// //     // code here runs if condition evaluates to false
+// // }else if (condition2){
+// //     // code here runs if condition1 is false *and* condition2 evaluates to true
+// // }else {
+// //     // code here gets executed if condition1 nor condition2 evaluate to true
 // // }
 //
+// var areYouOfAge = confirm("Are you at least 13 years of age or older?")
+//
+// if(areYouOfAge){
+//     alert("You may proceed!");
+// } else {
+//     alert("Sorry, you are not able to proceed.")
+// }
 
 
-// =============== IF / ELSE STATEMENT EXAMPLES ================
-//TODO Together: Show a specific navbar if user is admin, else show a different navbar
+// =============== IF / ELSE IF/ELSE STATEMENT EXAMPLES ================
+//TODO Together: Write an if/else if/else statement that alerts "It's snowing!" if weather is equal to "snowing", alerts "It's raining" if weather is equal to "raining", alerts "have a nice day" for all other cases
 
-var isAdmin = false;
+var weather = "earthquake"
 
-    if(isAdmin){
-        //show the admin navbar
-        console.log("is admin")
-    } else {
-        //show regular navbar
-        console.log("regular user")
-    }
-
-//TODO Together: Write an if statement that alerts "It's raining" if isRainy is true, else return "have a nice day!"
-
-var isRainy = true;
-
-if(isRainy){
+if(weather === "snowing"){
+    alert("It's snowing!")
+} else if (weather === "raining"){
     alert("It's raining!")
 } else {
     alert("Have a nice day!")
@@ -124,88 +224,21 @@ if(isRainy){
 
 
 
-//TODO: Write an if statement that alerts "Sorry, game over" if numberOfLives is 0, else alert "Next Level!"
+//TODO Together: refactor the above statement as a function
 
-var numberOfLives = 5;
-
-if(numberOfLives === 0){
-    alert("Sorry, game over!")
-} else {
-    alert("Next Level!")
-}
-
-
-//TODO: Write an if statement that alerts "It's snowing!" if weather is equal to "snowing", else alert "Check back later for more details!"
-
-var weather = "snowing";
-
-if(weather === "snowing"){
-    alert("It's snowing!")
-} else {
-    alert("Check back later for more details!")
-}
-
-
-
-
-//TODO: Write an if statement that alerts true if numberInput is greater than 10, else alert "the number is less than 10"
-
-var numberInput = 75
-
-if (numberInput > 10) {
-    alert("The number is greater than 10.")
-} else {
-    alert ("The number is less than 10.")
-}
-
-//TODO: Refactor todo#1 as a function named checkIfGameIsOver, that accepts numberOfLives and returns "Sorry, game over" if numberOfLives is 0, else return "Next Level!.
-
-function checkIfGameIsOver(numberOfLives) {
-    if(numberOfLives ===0) {
-        return "Sorry game over!";
+function checkWeather(weather){
+    if (weather === "snowing"){
+        return("It's snowing!")
+    } else if (weather === "raining"){
+        return("It's raining!")
     } else {
-        return "Next level!"
+        return("Have a nice day!")
     }
 }
 
-
-
-//TODO: EXTRA BONUS - Refactor todo#2 from above as a function.
-
-
-
-// SHOULD WE DELETE STUFF EXAMPLE
-var weShouldDeleteStuff = confirm("Are you sure you want to delete everything?")
-
-// ok selection ==> true, cancel selection ==> false
-
-if(weShouldDeleteStuff){
-    // delete everything
-    // double check
-    alert("We are deleting everything...");
-} else {
-    alert("Operation cancelled.")
-}
-
-
-
-//TODO: Write a confirm asking the user if they are 13 years of age or older. If they are, alert "You may proceed", if they are not, alert "Sorry, you are not able to proceed"
-
-// ================ IF / ELSE IF / ELSE STATEMENT SYNTAX ===============
-// if(condition1){
-//     // code here runs if condition evaluates to true
-// }else if (condition2){
-//     // code here runs if condition1 is false *and* condition2 evaluates to true
-// }else {
-//     // code here gets executed if condition1 nor condition2 evaluate to true
-// }
-
-
-
-// =============== IF / ELSE IF/ELSE STATEMENT EXAMPLES ================
-//TODO Together: Write an if/else if/else statement that alerts "It's snowing!" if weather is equal to "snowing", alerts "It's raining" if weather is equal to "raining", alerts "have a nice day" for all other cases
-
-//TODO Together: refactor the above statement as a function
+console.log(checkWeather("raining"));
+console.log(checkWeather("tornado"));
+console.log(checkWeather("enflamed"));
 
 // Together: PIZZA PREFERENCE EXAMPLE
 
