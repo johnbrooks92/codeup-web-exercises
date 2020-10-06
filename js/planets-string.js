32081c603367a8a30e5764cc2537d74b81033cce
John <johntbrooks92@gmail.com>
2020-10-06 14:03:30 -0500

    Almost Complete Exercises

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,40 +1,37 @@
 (function(){
     "use strict";
 
-    var planets = [
-        'Mercury',
-        'Venus',
-        'Earth',
-        'Mars',
-        'Jupiter',
-        'Saturn',
-        'Uranus',
-        'Neptune'
-    ];
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray = planetsString.split("_");
 
     /**
      * TODO:
-     * Read each console log below, and write some javascript code to perform
-     * the step that it describes
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
      */
 
-    console.log('Adding "The Sun" to the beginning of the planets array.');
-    console.log(planets);
+    // console.log(planetsArray);
 
-    console.log('Adding "Pluto" to the end of the planets array.');
-    console.log(planets);
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
 
-    console.log('Removing "The Sun" from the beginning of the planets array.');
-    console.log(planets);
+     * BONUS:
+     * Create another string that would display your planets in an unordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
 
-    console.log('Removing "Pluto" from the end of the planets array.');
-    console.log(planets);
+    var planetsBroken = planetsArray.join("<br>")
+    console.log(planetsBroken);
 
-    console.log('Finding and logging the index of "Earth" in the planets array.');
+    var planetsSplit = planetsBroken.split("|")
+    console.log(planetsSplit);
 
-    console.log("Reversing the order of the planets array.");
-    console.log(planets);
-
-    console.log("Sorting the planets array.");
-    console.log(planets);
+    planetsSplit.unshift('<ul>');
+    planetsSplit.push('<ul>');
+    console.log(planetsSplit);
 })();
+
