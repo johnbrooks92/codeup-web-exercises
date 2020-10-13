2edf0a4edc8c0577a714e169898f669ec5f35f39
John <johntbrooks92@gmail.com>
2020-10-13 09:31:31 -0500

    Warmup 10.12.2020

--- js/notes.js
+++ js/notes.js
@@ -1,1 +1,43 @@
 "use strict";
+
+
+// # HTML Elements Pre Lesson
+// ### Overview
+// 1. In your own words, define an HTML element.
+//  HTML Elements are describe data so that our web document interprets and executes the content within in a specific manner.
+//
+// 2. What is a void element?
+//      A void element does not contain content, usually just executes a command. Does not require a closing tag. (Think <br> for line breaks)
+//     3. What do all elements begin and end with
+//        All elements begin and end within <>.
+//     4. What is the syntax of an HTML element
+//             <element></element>
+// 5. What do we call an element that contains other elements? What are the nested elements called?
+//          The surrounding element is called the parent element and the nested elements are called child elements.
+//
+//     ### Block-Level vs. Inline Elements
+//
+//
+// 1. What is a block level element? What is an example of a block level element?
+//      A block-level element begins on a new line and occupy the entire space of it's parent element.
+//     2. What is an inline element? What is an example of an inline element?
+//      An inline element occupies only the space bounded by the tags defining it.
+//     ### Attributes
+// 1. What is an attribute?
+//      An attribute is written inside of the opening element to perform a modified purpose. (think src="" in script tags to link JS files)
+//     ### Common HTML Elements
+
+// Experiment with the following elements. What are some things you notice about these?
+
+// *  Headings - Block- Level. Come in multiple fashions labeled h1-h6 for different font sizes.
+// *  Paragraphs - Block-level. Formats text within to paragraph.
+// *  Line Breaks - block-level and void. Creates a line break.
+// *  Horizontal Rule - block-level and void. Does the same thing as <br> but with a horizontal line used.
+// *  Strong - Inline element. Makes your font bold.
+// *  Emphasis - Inline element. Makes your font italicized.
+// *  Nesting Elements - Used to apply inline elements within a block element.
+// *  Lists (Ordered & Unordered) - Block-level. Unordered list provides a list with bullet points. Ordered list provides the list numbered.
+// *  Anchor Tag - Inline element. Hyperlinks other pages. Contents, including nested elements, will follow link.
+//                  href = location or navigate to. target attribute specifies how a link opens.
+// *  Images = Inline and void element. Displays an img. src attribute specifies image URL, alt specifies alternate text.
+// *  Tables = Used to display tabular data. tr = table row, th = table heading, td = tables cells.
