fd341b137ea7e67594c255ba7d5a24ee02091a10
John <johntbrooks92@gmail.com>
2020-11-23 10:03:17 -0600

    mfr exercise updated

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -37,3 +37,38 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+const filteredUsers = users.filter((x) => x.languages.length >= 3);
+// const filteredUsers = users.filter(x => x.languages.length >= 3);
+// This method works, but only because there is only one input being called in the function. If there were two, use uncommented version.
+console.log(filteredUsers);
+
+const emails = users.map((user) => user.email);
+console.log(emails);
+
+const totalYears = users.reduce((a, b) => a + b.yearsOfExperience, 0);
+console.log(`total years of combined experience is ${totalYears}`);
+console.log(`average length of experience is ${totalYears/users.length}`);
+
+const longestEmail = users.reduce((currentEmail, user) => {
+    if(currentEmail.length < user.email.length){
+        currentEmail = user.email;
+    }
+    return currentEmail;
+}, "");
+console.log(longestEmail);
+
+
+let startingString = "Your instructors are: ";
+const instructors = users.reduce((a, b) => `${a} ${b.name},`, startingString);
+console.log(instructors.substring(0, instructors.length -1) + ".");
+
+
+const uniqueLang = users.reduce((arr, user) => {
+    user.languages.forEach((lang) => {
+        if(typeof arr[lang] === "undefined"){
+            arr[lang] = "unique";
+        }
+    });
+    return arr;
+},{});
+console.log(uniqueLang);
